611c08a01abee61b827ae14200d66e24790e5fbd
Leonardo Cserny <leo.cserny@gmail.com>
2024-09-17 10:30:54 +0300

    minor fix

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,7 @@ async function bootstrap() {
     app.setGlobalPrefix(config.application.path);
     app.enableCors({
         credentials: true,
-        // TODO: get from config
-        origin: "http://localhost:4200",
+        origin: true,
         methods: ['GET', 'POST', 'PUT', 'PATCH', 'OPTIONS', 'DELETE'],
         allowedHeaders: ["Content-Type", "Authorization"]
     });
