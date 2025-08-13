8c8588d1f10a7f8f8f6185ec01bc2ffc069bb0c3
Leonardo Cserny <leo.cserny@gmail.com>
2025-08-13 23:27:47 +0300

    impl SSL

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,16 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { CurrentConfig } from './current.config';
 import { AppLogger } from './app.logger';
-import { ValidationPipe } from '@nestjs/common';
+import { NestApplicationOptions, ValidationPipe } from '@nestjs/common';
 import { HttpErrorHandler } from './error.handler';
+import * as fs from 'node:fs';
+
+const enableSSL = "ENABLE_SSL";
+const certPath = "SSL_CERT_PATH";
+const keyPath = "SSL_KEY_PATH";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, {
-        bufferLogs: true
-    });
+    const app = await createApp();
     const config = app.get(CurrentConfig);
     const logger = app.get(AppLogger);
 
@@ -33,6 +36,31 @@ async function bootstrap() {
     logger.log(`${config.application.name} starting on port: ${config.application.port}`);
 
     await app.listen(config.application.port, '0.0.0.0');
+}
+
+async function createApp() {
+    const isHttps = process.env[enableSSL] === 'true';
+    const options: NestApplicationOptions = { bufferLogs: true };
+
+    if (isHttps) {
+        options.httpsOptions = {
+            key: fs.readFileSync(getMandatoryEnvVar(keyPath), 'utf8'),
+            cert: fs.readFileSync(getMandatoryEnvVar(certPath), 'utf8'),
+        };
+        console.log('🚀 Server running with SSL');
+    } else {
+        console.log('🚀 Server running without SSL');
+    }
+
+    return NestFactory.create(AppModule, options);
+}
+
+function getMandatoryEnvVar(name: string): string {
+    const value = process.env[name];
+    if (!value) {
+        throw new Error(`Missing required env var: ${name}`);
+    }
+    return value;
 }
 
 bootstrap();
